497bd39047cc2207fa1a50843ca58b7d352ee79f
Trevor Steil <trevor.steil1@gmail.com>
2022-09-19 17:28:10 -0700

    Adds example building Kronecker graph from file

--- src/kronecker_example.cpp
+++ src/kronecker_example.cpp
@@ -9,16 +9,35 @@
 int main(int argc, char** argv) {
   ygm::comm world(&argc, &argv);
 
-  std::vector<std::tuple<uint64_t, uint64_t, uint32_t>> g1{{0, 1, 1},
-                                                           {1, 0, 2}};
-  std::vector<std::tuple<uint64_t, uint64_t, uint32_t>> g2{
-      {0, 1, 2}, {1, 1, 3}, {2, 0, 1}};
+  {  // Construct from graph in memory
+    std::vector<std::tuple<uint64_t, uint64_t, uint32_t>> g1{{0, 1, 1},
+                                                             {1, 0, 2}};
+    std::vector<std::tuple<uint64_t, uint64_t, uint32_t>> g2{
+        {0, 1, 2}, {1, 1, 3}, {2, 0, 1}};
 
-  kronecker_edge_generator kron(world, g1, g2, 2, 3);
+    kronecker_edge_generator kron(world, g1, g2, 2, 3);
 
-  kron.for_all([&world](const auto row, const auto col, const auto val) {
-    world.cout() << "(" << row << ", " << col << ") : " << val << std::endl;
-  });
+    kron.for_all([&world](const auto row, const auto col, const auto val) {
+      world.cout() << "(" << row << ", " << col << ") : " << val << std::endl;
+    });
+  }
+
+  if (argc == 3) {  // Construct from graph in file
+    std::string graphA_filename(argv[1]);
+    std::string graphB_filename(argv[2]);
+
+    kronecker_edge_generator kron(world, graphA_filename, graphB_filename);
+
+    uint64_t num_edges{0};
+
+    kron.for_all([&num_edges](const auto row, const auto col, const auto val) {
+      ++num_edges;
+    });
+
+    world.barrier();
+
+    world.cout0("Kronecker edges: ", world.all_reduce_sum(num_edges));
+  }
 
   return 0;
 }
